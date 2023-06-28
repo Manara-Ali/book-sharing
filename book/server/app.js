f38e8bb4d7ffaf99581ca9241cfa07f430d09cc0
Manara-Ali <manaraali64@gmail.com>
2023-06-28 13:59:30 -0400

    Add error handling to our application

--- book/server/app.js
+++ book/server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const bookRouter = require("./routes/bookRoutes");
+const errorController = require("./controllers/errorController");
 
 const app = express();
 
@@ -16,5 +17,17 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use("/api/v1/books", bookRouter);
+
+app.all("*", (req, res, next) => {
+  //   const error = new Error(`Cannot find ${req.originalUrl} on our servers...`);
+  const applicationError = new ApplicationError(
+    `Cannot find ${req.originalUrl} on any of our servers...`,
+    404
+  );
+
+  next(applicationError);
+});
+
+app.use(errorController.globalErrorHandler);
 
 module.exports = app;
