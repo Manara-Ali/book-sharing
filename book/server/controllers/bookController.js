bc0f20170ee9dc8acbd702c1bea82d03f3180477
Manara-Ali <manaraali64@gmail.com>
2023-06-28 10:04:46 -0400

    Create controller to retrieve the list of all the books

--- book/server/controllers/bookController.js
+++ book/server/controllers/bookController.js
@@ -1,9 +1,14 @@
-exports.getAllBooks = (req, res, next) => {
+const Book = require("../models/bookModel");
+
+exports.getAllBooks = async (req, res, next) => {
+  // Return document by awaiting query
+  const books = await Book.find();
+
   res.status(200).json({
     status: "success",
-    results: "Number of books",
+    results: books.length,
     data: {
-      message: "book data",
+      books,
     },
   });
 };
