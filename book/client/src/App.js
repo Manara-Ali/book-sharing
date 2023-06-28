a5a8a6e7dc69fb63ea4d89e8063f7be73dbdc517
Manara-Ali <manaraali64@gmail.com>
2023-06-28 16:21:36 -0400

    Change main-container from class to id attribute

--- book/client/src/App.js
+++ book/client/src/App.js
@@ -1,11 +1,12 @@
 import "./bootstrap.min.css";
+import "./App.css";
 import React from "react";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
+    <div id="main-container">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
