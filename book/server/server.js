3dec0d0f09489c4bc8998f9b44983df0e2137046
Manara-Ali <manaraali64@gmail.com>
2023-06-28 10:26:05 -0400

    Create a server file to hold all servers information

--- book/server/server.js
+++ book/server/server.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config({
@@ -5,6 +6,14 @@ dotenv.config({
 });
 
 const app = require("./app");
+
+const db = mongoose
+  .connect(
+    process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD)
+  )
+  .then((con) => {
+    console.log("DATABASE CONNECTION SUCCESSFUL!");
+  });
 
 const port = process.env.PORT;
 
