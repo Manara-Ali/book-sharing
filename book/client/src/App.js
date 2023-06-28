8b00806543bfe0259a7d07d1018b9e7b46c92342
Manara-Ali <manaraali64@gmail.com>
2023-06-28 16:49:51 -0400

    Import Navbar component

--- book/client/src/App.js
+++ book/client/src/App.js
@@ -1,16 +1,20 @@
 import "./bootstrap.min.css";
 import "./App.css";
 import React from "react";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div id="main-container">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    <>
+      <Navbar />
+      <div id="main-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </>
   );
 };
 
