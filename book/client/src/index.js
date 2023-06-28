f301becc254a0e4770c5efa75f448b5266487618
Manara-Ali <manaraali64@gmail.com>
2023-06-28 15:26:47 -0400

    Adding browserRouter

--- book/client/src/index.js
+++ book/client/src/index.js
@@ -1,6 +1,11 @@
 import "./bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.js";
 
-ReactDOM.createRoot(document.querySelector("#root")).render(<App />);
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <Router>
+    <App />
+  </Router>
+);
