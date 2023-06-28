fdef7a97bd74a38e0963c60a0dd42574cd8efdf3
Manara-Ali <manaraali64@gmail.com>
2023-06-28 15:25:57 -0400

    Import Home component

--- book/client/src/App.js
+++ book/client/src/App.js
@@ -1,9 +1,14 @@
+import "./bootstrap.min.css";
 import React from "react";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <h1>App Component</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
