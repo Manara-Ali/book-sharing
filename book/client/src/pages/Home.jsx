f5613fd1d7240112f8411a4363cfd6e154a87f1f
Manara-Ali <manaraali64@gmail.com>
2023-06-29 10:07:09 -0400

    Load books on load

--- book/client/src/pages/Home.jsx
+++ book/client/src/pages/Home.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "react-bootstrap";
 import { HiOutlinePlus } from "react-icons/hi";
 import { Link } from "react-router-dom";
+import { fetchBooks } from "../store";
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  const data = useSelector((state) => {
+    console.log(state.booksCombinedReducer);
+    return;
+  });
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
   return (
     <div className="header">
       <div className="book-review-header">
