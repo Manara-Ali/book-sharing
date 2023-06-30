66c32869d29cb69b20e740d2e16c32ef5c16a978
Manara-Ali <manaraali64@gmail.com>
2023-06-30 14:45:36 -0400

    Update book detail component

--- book/client/src/pages/BookDetail.jsx
+++ book/client/src/pages/BookDetail.jsx
@@ -2,10 +2,37 @@ import React, { useState, useEffect, useContext } from "react";
 import { useParams, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { Button } from "react-bootstrap";
+import { fetchBook } from "../store";
 
 const BookDetail = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
+  const {
+    title,
+    genre,
+    author,
+    numberOfPages,
+    numberOfChapters,
+    ratingsAverage,
+    ratingsQuantity,
+    synopsis,
+    myExplanation,
+    finalThoughts,
+    recommend,
+    coverImage,
+    images,
+    createdAt,
+    reviews,
+    userId,
+  } = useSelector((state) => {
+    return state.booksCombinedReducer.book;
+  });
+
+  const date = createdAt && new Date(createdAt);
+
+  useEffect(() => {
+    dispatch(fetchBook(id));
+  }, [id, dispatch]);
 
   return (
     <>
@@ -22,20 +49,31 @@ const BookDetail = () => {
           />
           <div className="book-detail">
             <h1>Book Detail Page</h1>
-            <p>Title:</p>
-            <p>Genre:</p>
-            <p>Author:</p>
-            <p>Pages:</p>
-            <p>Chapters:</p>
+            <p>Title:{title}</p>
+            <p>Genre:{genre}</p>
+            <p>Author:{author}</p>
+            <p>Pages:{numberOfPages}</p>
+            <p>Chapters:{numberOfChapters}</p>
             <p>
-              Ratings:
+              Ratings:{ratingsAverage} ({ratingsQuantity})
             </p>
-            <p>Synopsis:</p>
-            <p>My Understanding of the Story Line:</p>
-            <p>Final Thoughts:</p>
-            <p>Would you recommend this book?:</p>
+            <p>Synopsis:{synopsis}</p>
+            <p>My Understanding of the Story Line:{myExplanation}</p>
+            <p>Final Thoughts:{finalThoughts}</p>
+            <p>Would you recommend this book?:{recommend && "YES"}</p>
+            <p>
+              Created:{" "}
+              {date
+                ? Intl.DateTimeFormat("en-US", {
+                    weekday: "short",
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                  }).format(date)
+                : "Loading..."}
+            </p>
             <div className="image-list-container">
-              Images
+              User's Images
             </div>
           </div>
         </div>
