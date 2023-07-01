5c8ddde90613ee7f7c07006364a71ca4e6907bab
Manara-Ali <manaraali64@gmail.com>
2023-07-01 16:07:09 -0400

    Create controllers to login and logout users

--- book/server/controllers/authController.js
+++ book/server/controllers/authController.js
@@ -13,4 +13,46 @@ exports.signup = catchAsyncFn(async (req, res, next) => {
     });
   
     createAndSendToken(user, 201, res);
-  });
+});
+
+
+exports.login = catchAsyncFn(async (req, res, next) => {
+    // 1. Retrieve email && password
+    const { email, password } = req.body;
+  
+    if (!email || !password) {
+      const applicationError = new ApplicationError(
+        "Email and password are required to log in",
+        400
+      );
+  
+      next(applicationError);
+    }
+  
+    // 2. Compare email and password to one in db
+    const user = await User.findOne({ email }).select("+password");
+  
+    if (!user || !(await user.comparePassword(password, user.password))) {
+      const applicationError = new ApplicationError(
+        "Invalid email or password. Try again",
+        401
+      );
+  
+      next(applicationError);
+  
+      return;
+    }
+  
+    createAndSendToken(user, 200, res);
+});
+
+exports.logout = (req, res, next) => {
+    res.cookie("jwt", "", {
+      expires: new Date(Date.now() - 10000),
+      httpOnly: true,
+    });
+  
+    res.status(200).json({
+      status: "success",
+    });
+};
