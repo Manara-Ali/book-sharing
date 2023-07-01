496ac75cc1db9eeae474d99ed09ca2ca85502ef6
Manara-Ali <manaraali64@gmail.com>
2023-07-01 12:15:58 -0400

    Add a fullfilled case to fetch single book in db

--- book/client/src/store/slices/bookSlice.js
+++ book/client/src/store/slices/bookSlice.js
@@ -1,16 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
-import {fetchBooks} from "../index";
+import {fetchBooks, fetchBook} from "../index";
 
 const bookSlice = createSlice({
     name: "books",
     initialState: {
         data: [],
+        book: {},
     },
     reducers: {},
     extraReducers(builder) {
         builder.addCase(fetchBooks.fulfilled, (state, action) => {
             state.data = action.payload.books;
-        })
+        });
+
+        builder.addCase(fetchBook.fulfilled, (state, action) => {
+            state.book = action.payload.book;
+        });
     }
 });
 
