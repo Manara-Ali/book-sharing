511f91fc2413d5d654e376864ed217bee4a804b7
Manara-Ali <manaraali64@gmail.com>
2023-07-01 13:47:28 -0400

    Add signup to browser router

--- book/client/src/App.js
+++ book/client/src/App.js
@@ -4,7 +4,9 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
 import BookDetail from "./pages/BookDetail";
+import Signup from "./pages/Signup";
 
 const App = () => {
   return (
@@ -13,7 +15,9 @@ const App = () => {
       <div id="main-container">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
           <Route path="/:id" element={<BookDetail />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </>
