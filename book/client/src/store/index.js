24ac7d2d69b1f0954bb2e76836962539fcc5776d
Manara-Ali <manaraali64@gmail.com>
2023-07-01 12:15:03 -0400

    Import fetchbook thunk to store

--- book/client/src/store/index.js
+++ book/client/src/store/index.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { fetchBooks } from "./thunks/fetchBooks";
+import { fetchBook } from "./thunks/fetchBook";
 import { booksCombinedReducer } from "./slices/bookSlice";
-export {fetchBooks}
+export {fetchBooks, fetchBook}
 
 export const store = configureStore({
     reducer: {
