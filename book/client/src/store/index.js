64c666c8d5a52abed847accab75d26cc837a7faa
Manara-Ali <manaraali64@gmail.com>
2023-07-01 16:09:23 -0400

    Import login and logout thunk

--- book/client/src/store/index.js
+++ book/client/src/store/index.js
@@ -2,6 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import { fetchBooks } from "./thunks/fetchBooks";
 import { fetchBook } from "./thunks/fetchBook";
 import { signupUser } from "./thunks/signupUser";
+import { loginUser } from "./thunks/loginUser";
+import { logoutUser } from "./thunks/logoutUser";
 import { booksCombinedReducer } from "./slices/bookSlice";
 import { formCombinedReducer } from "./slices/formSlice";
 import { usersCombinedReducer } from "./slices/userSlice";
@@ -19,6 +21,8 @@ export {
     fetchBooks,
     fetchBook,
     signupUser,
+    loginUser,
+    logoutUser,
     enteredName,
     enteredEmail,
     enteredPassword,
