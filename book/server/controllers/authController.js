1c9adcda4d6c45a43695b5e4907785bf8de07fe7
Manara-Ali <manaraali64@gmail.com>
2023-07-01 16:45:55 -0400

    Create controller for creating new books

--- book/server/controllers/authController.js
+++ book/server/controllers/authController.js
@@ -56,3 +56,130 @@ exports.logout = (req, res, next) => {
       status: "success",
     });
 };
+
+// Create a middleware to determine if the user is loggedin
+exports.isLoggedIn = catchAsyncFn(async (req, res, next) => {
+    if (req.cookies.jwt) {
+      const decodedPayload = await util.promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET_KEY
+      );
+  
+      const currentUser = await User.findById(decodedPayload.id).select(
+        "+password +passwordChangedAt +role"
+      );
+  
+      if (!currentUser) {
+        next();
+        return;
+      }
+  
+      // 6. Verify that the password was not recently changed
+      if (currentUser.wasPasswordChanged(decodedPayload.iat)) {
+        next();
+        return;
+      }
+  
+      // 7. Attach user to request object
+      currentUser.password = undefined;
+      currentUser.role = undefined;
+  
+      res.loggedInUser = currentUser;
+  
+      // console.log("HERE", res.loggedInUser);
+  
+      next();
+  
+      return;
+    }
+  
+    // res.loggedInUser = null;
+    res.loggedInUser = {};
+  
+    // console.log("THERE", res.loggedInUser);
+  
+    next();
+});
+
+// Create a function to protect given routes
+exports.protect = catchAsyncFn(async (req, res, next) => {
+    let token;
+  
+    // 1. Retrieve token from request headers
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    } else if (req.cookies.jwt) {
+      token = req.cookies.jwt;
+    }
+  
+    if (!token) {
+      const applicationError = new ApplicationError(
+        "You are not logged in. Please log in and try again",
+        401
+      );
+  
+      next(applicationError);
+  
+      return;
+    }
+  
+    // 2. Find the user associated with the token
+    const decodedPayload = await util.promisify(jwt.verify)(
+      token,
+      process.env.JWT_SECRET_KEY
+    );
+  
+    // 3. Verifty that user still exist
+    const user = await User.findById(decodedPayload.id).select(
+      "+password +passwordChangedAt +role"
+    );
+  
+    if (!user) {
+      const applicationError = new ApplicationError(
+        "This user is no longer a part of our community. Please log back in.",
+        401
+      );
+  
+      next(applicationError);
+    }
+  
+    // 4. JWT ERROR
+    // 5. TOKEN EXPIRED ERROR
+  
+    // 6. Verify that the password was not recently changed
+    if (user.wasPasswordChanged(decodedPayload.iat)) {
+      const applicationError = new ApplicationError(
+        "Your password was recently changed. Please log back in with new password and try again.",
+        401
+      );
+  
+      next(applicationError);
+  
+      return;
+    }
+  
+    // 7. Attach user to request object
+    req.user = user;
+  
+    next();
+});
+
+exports.restrictTo = (...args) => {
+    return (req, res, next) => {
+      if (!args.includes(req.user.role)) {
+        const applicationError = new ApplicationError(
+          "You do not have permission to access this resource.",
+          403
+        );
+  
+        next(applicationError);
+  
+        return;
+      }
+  
+      next();
+    };
+};
