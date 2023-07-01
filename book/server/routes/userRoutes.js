a7ad9154e73cb134e58e412b0c807f1872ef7178
Manara-Ali <manaraali64@gmail.com>
2023-07-01 16:05:25 -0400

    Create route to login and logout users

--- book/server/routes/userRoutes.js
+++ book/server/routes/userRoutes.js
@@ -4,5 +4,7 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.route("/signup").post(authController.signup);
+router.route("/login").post(authController.login);
+router.route("/logout").get(authController.logout);
 
 module.exports = router;
