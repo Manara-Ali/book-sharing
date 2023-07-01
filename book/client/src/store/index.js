ec67d2e7a4d17484419cc7a0974f3b620f446146
Manara-Ali <manaraali64@gmail.com>
2023-07-01 14:38:01 -0400

    Import user combined reducer

--- book/client/src/store/index.js
+++ book/client/src/store/index.js
@@ -4,12 +4,14 @@ import { fetchBook } from "./thunks/fetchBook";
 import { signupUser } from "./thunks/signupUser";
 import { booksCombinedReducer } from "./slices/bookSlice";
 import { formCombinedReducer } from "./slices/formSlice";
+import { usersCombinedReducer } from "./slices/userSlice";
 import { enteredName, enteredEmail, enteredPassword, enteredPasswordConfirm } from "./slices/formSlice";
 
 export const store = configureStore({
     reducer: {
         booksCombinedReducer,
         formCombinedReducer,
+        usersCombinedReducer,
     }
 });
 
