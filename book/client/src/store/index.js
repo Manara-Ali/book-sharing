4f40e3902d3778c6d6bd2fbc43a2feca8c8a7f8e
Manara-Ali <manaraali64@gmail.com>
2023-07-01 14:31:18 -0400

    Import signup thunk into the store

--- book/client/src/store/index.js
+++ book/client/src/store/index.js
@@ -1,11 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { fetchBooks } from "./thunks/fetchBooks";
 import { fetchBook } from "./thunks/fetchBook";
+import { signupUser } from "./thunks/signupUser";
 import { booksCombinedReducer } from "./slices/bookSlice";
-export {fetchBooks, fetchBook}
+import { formCombinedReducer } from "./slices/formSlice";
+import { enteredName, enteredEmail, enteredPassword, enteredPasswordConfirm } from "./slices/formSlice";
 
 export const store = configureStore({
     reducer: {
         booksCombinedReducer,
+        formCombinedReducer,
     }
 });
+
+export {
+    fetchBooks,
+    fetchBook,
+    signupUser,
+    enteredName,
+    enteredEmail,
+    enteredPassword,
+    enteredPasswordConfirm
+}
