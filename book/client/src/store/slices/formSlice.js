9dd4313d30617e0b207b3ab7564c35723d05b638
Manara-Ali <manaraali64@gmail.com>
2023-07-01 14:28:19 -0400

    Create form slice

---
+++ book/client/src/store/slices/formSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { signupUser } from "..";
+
+const formSlice = createSlice({
+    name: "form",
+    initialState: {
+        name: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+    },
+    reducers: {
+        enteredName(state, action) {
+            state.name = action.payload;
+        },
+        enteredEmail(state, action) {
+            state.email = action.payload;
+        },
+        enteredPassword(state, action) {
+            state.password = action.payload;
+        },
+        enteredPasswordConfirm(state, action) {
+            state.passwordConfirm = action.payload;
+        },
+        enteredCurrentPassword(state, action) {
+            state.currentPassword = action.payload;
+        },
+    },
+    extraReducers(builder) {
+        builder.addCase(signupUser.fulfilled, (state, action) => {
+            state.name = "";
+            state.email = "";
+            state.password = "";
+            state.passwordConfirm = "";
+        })
+    }
+});
+
+// Create action creators
+export const {
+    enteredName,
+    enteredEmail,
+    enteredPassword,
+    enteredPasswordConfirm,
+} = formSlice.actions;
+
+// Creation combined reducers
+export const formCombinedReducer = formSlice.reducer;
