30adb6a67558b1efb810fa98982a998796fd5891
Manara-Ali <manaraali64@gmail.com>
2023-07-04 13:16:30 -0400

    Change url from production to development testing

--- book/client/src/store/thunks/loginUser.js
+++ book/client/src/store/thunks/loginUser.js
@@ -4,16 +4,13 @@ import axios from "axios";
 export const loginUser = createAsyncThunk(
   "user/login",
   async (userData, { rejectWithValue }) => {
-      console.log(userData)
     try {
       const response = await axios({
-        // url: "/api/v1/users/login",
-        url: "https://books-jx67.onrender.com/api/v1/users/login", // LIVE SITE
+        url: "/api/v1/users/login",
+        // url: "https://books-jx67.onrender.com/api/v1/users/login", // LIVE SITE
         method: "POST",
         data: userData,
       });
-
-      console.log(response);
 
       const { data } = response.data;
 
