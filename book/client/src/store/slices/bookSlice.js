1fb4f0940b5abb4b86c2df2b50a593aecbe52b06
Manara-Ali <manaraali64@gmail.com>
2023-07-04 15:10:10 -0400

    Add builder case to create book fulfille

--- book/client/src/store/slices/bookSlice.js
+++ book/client/src/store/slices/bookSlice.js
@@ -1,21 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
-import {fetchBooks, fetchBook} from "../index";
+import {fetchBooks, fetchBook, createBook} from "../index";
 
 const bookSlice = createSlice({
     name: "books",
     initialState: {
         data: [],
         book: {},
+        status: "",
     },
     reducers: {},
     extraReducers(builder) {
         builder.addCase(fetchBooks.fulfilled, (state, action) => {
             state.data = action.payload.books;
+            state.status = "";
         });
 
         builder.addCase(fetchBook.fulfilled, (state, action) => {
             state.book = action.payload.book;
         });
+
+        builder.addCase(createBook.fulfilled, (state, action) => {
+            // CHECK THIS LINE OF CODE!!!!!
+            console.log(action.payload);
+            state.status = action.payload.status;
+          });
     }
 });
 
